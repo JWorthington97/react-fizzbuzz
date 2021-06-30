1b100218573a48a44f5d06882f0fe01d7e79512e
JWorthington97 <jb.worthington@live.co.uk>
2021-06-30 10:22:26 +0100

    Implement fizzbuzz formatted

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { greet } from "./utils/greet";
 import FizzBuzz from "./Fizzbuzz";
 
 function App(): JSX.Element {
-  return <>
-  <h1>{greet("World")}</h1>
-  <FizzBuzz />
-  </>
+  return (
+    <>
+      <h1>{greet("World")}</h1>
+      <FizzBuzz />
+    </>
+  );
 }
 
 export default App;
